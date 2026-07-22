1f79b985045b3d9b53c7f051411a07b1efa3cd03
yishiki <83524865+yoshito-ishiki-math@users.noreply.github.com>
2026-07-22 13:13:56 +0900

    Add implementation for `kicho doctor` command

---
+++ lib/kicho/commands/doctor.sh
@@ -0,0 +1,9 @@
+# Implementation of `kicho doctor`.
+
+kicho_command_doctor_summary() {
+    printf 'Check the local Kicho environment.\n'
+}
+
+kicho_command_doctor() {
+    printf 'Doctor is working!\n'
+}
