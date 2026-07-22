bd358f3fb6e9b9f6710f7f8be5769a632d866886
yishiki <83524865+yoshito-ishiki-math@users.noreply.github.com>
2026-07-22 13:50:27 +0900

    Implement archive source copying functionality in `kicho archive` command

--- lib/kicho/commands/archive.sh
+++ lib/kicho/commands/archive.sh
@@ -42,6 +42,28 @@ kicho_archive_create_directories() {
         "$root/metadata"
 }
 
+kicho_archive_copy_if_exists() {
+    local source="$1"
+    local destination="$2"
+
+    if [[ -e "$source" ]]; then
+        cp -R "$source" "$destination"
+    else
+        printf 'Warning: %s not found.\n' "$source" >&2
+    fi
+}
+
+kicho_archive_copy_source() {
+    local destination="$1"
+
+    kicho_archive_copy_if_exists main.tex "$destination"
+    kicho_archive_copy_if_exists sections "$destination"
+    kicho_archive_copy_if_exists preamble "$destination"
+    kicho_archive_copy_if_exists figures "$destination"
+    kicho_archive_copy_if_exists bib "$destination"
+    kicho_archive_copy_if_exists .latexmkrc "$destination"
+}
+
 kicho_command_archive() {
     if [[ $# -ne 0 ]]; then
         kicho_error "archive does not accept arguments."
@@ -56,6 +78,7 @@ kicho_command_archive() {
     archive_root="$(kicho_archive_root "$timestamp")"
 
     kicho_archive_create_directories "$archive_root"
+    kicho_archive_copy_source "$archive_root/source"
 
     printf 'Created archive:\n'
     printf '    %s\n' "$archive_root"
